3d153c53ad5d14cf346ba76f756e4aabbfdbbdbe
Harald Grove <haraldgrove@gmail.com>
2018-01-06 21:39:03 +0700

    Gui training

---
+++ feet2meters.py
@@ -0,0 +1,45 @@
+from tkinter import *
+from tkinter import ttk
+
+# Note: As soon as meters gets a new value, the display updates by itself
+def calculate(*args):
+    try:
+        value = float(feet.get())
+        meters.set((0.3048 * value * 10000.0 + 0.5) / 10000.0)
+    except ValueError:
+        pass
+
+
+root = Tk()
+root.title("Feet to Meters")
+
+mainframe = ttk.Frame(root, padding="3 3 12 12")
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+# These two variables will be attached to the widgets
+# Whenever these changes value, it will automaticall update the display.
+feet = StringVar()
+meters = StringVar()
+
+feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
+feet_entry.grid(column=2, row=1, sticky=(W, E))
+
+ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+
+ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children():
+    child.grid_configure(padx=5, pady=5)
+
+# Activates the entry field when the window opens.
+# Avoids having to click on the field to enter values.
+feet_entry.focus()
+# Binds the return/enter key to the calculate function
+root.bind('<Return>', calculate)
+
+root.mainloop()
